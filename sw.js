0a1e1e44ca9aafb847905d63076b1f6c30e83764
Adolfo Lopez <alopezcia@gmail.com>
2019-06-05 14:21:19 +0200

    Modificamos la ruta del SW

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const STATIC_CACHE = 'static-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
-    '/',
+    // '/',
     'index.html',
     'styles/certificate2.css',
     'styles/cube.css',
